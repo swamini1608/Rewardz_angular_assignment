e64b8c854ffe723e5aa41edab1d272dcdaa48a8e
swamini1608 <swamini.25@gmail.com>
2024-12-23 16:37:39 +0530

    did the basic html changes

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-content.component.css']
 })
 export class MainContentComponent {
+  accordionItems = [
+    { title: 'Accordion 1', content: 'This is the content of Accordion 1.' },
+    { title: 'Accordion 2', content: 'This is the content of Accordion 2.' },
+    { title: 'Accordion 3', content: 'This is the content of Accordion 3.' }
+  ];
 
+  selectedAccordion: number | null = null;
+
+  toggleAccordion(index: number): void {
+    this.selectedAccordion = this.selectedAccordion === index ? null : index;
+  }
+
+  isVisible: boolean = false;
+
+  // Method to show overlay
+  openOverlay() {
+    this.isVisible = true;
+  }
+
+  // Method to hide overlay
+  closeOverlay() {
+    this.isVisible = false;
+  }
 }
